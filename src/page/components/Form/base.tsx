00898717d7276fa07ee647c9a1190a9f21adee68
wangtomcat <1217149455@qq.com>
2021-03-17 20:49:32 +0800

    feat form的基本模版

---
+++ src/page/components/Form/base.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+export interface IProps {
+  data: any
+}
+
+interface IState {
+  value: any;
+  message: any;
+  defaultValue: any;
+  defaultMessage: any;
+  [key: string]: any;
+}
+
+
+export default class FormBase extends React.Component<IProps, IState>{
+  constructor(props: IProps) {
+    super(props)
+    const { data } = props
+    const defaultValue = data?.value;
+    const defaultMessage = data?.message
+
+    this.state = {
+      value: defaultValue,
+      message: defaultMessage,
+      defaultValue,
+      defaultMessage
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps: IProps, state: IState) {
+    const { data } = nextProps
+    const newState = state
+    if (JSON.stringify(state.defaultValue) != JSON.stringify(data.value)) {
+      newState.value = data.value
+      newState.defaultValue = data.value
+    }
+
+    if (state.defaultMessage != data.message) {
+      newState.message = data.message;
+      newState.defaultMessage = data.message;
+    }
+
+    return newState
+  }
+
+  setMessage(msg: any) {
+    this.setState({
+      message: msg
+    })
+  }
+
+  validate = (val: any) => {
+    const { validator } = this.props.data
+    if (validator) {
+      return true
+    }
+
+    const validateStatus = validator.validate(val)
+    const validMsg = validator.getMssage()
+    if (!validateStatus) {
+      this.setMessage(validMsg)
+    } else {
+      this.setMessage("")
+    }
+    return validateStatus
+  }
+
+  getData() {
+    const { data } = this.props
+    const { value } = this.state
+    const status = this.validate(value)
+    if (!status) {
+      return false
+    }
+    if (data.name || data.paramKey) {
+      return { [data.name || data.paramKey]: value }
+    }
+    return {}
+  }
+
+}
